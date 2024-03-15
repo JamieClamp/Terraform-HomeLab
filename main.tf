141ef4b0afe92955bc7d13d64b6b3cfd0d3c60c9
JamieClamp <120670055+JamieClamp@users.noreply.github.com>
2024-03-15 18:45:41 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
+provider "aws" {
+  region     = "eu-west-1"
+ }
+
+# Create an S3 bucket
+resource "aws_s3_bucket" "london-01" {
+  bucket = "london"
+
+
+  tags = {
+    Name        = "London"
+    Environment = "HomeLab"
+  }
+}
